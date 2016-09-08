ed478036cabd04e9909e21b2f1da8da1480f1d31
Evgeny Cherkashin <eugeneai@irnok.net>
2016-09-08 16:18:11 +0800

    Tried to use templates, failed.

--- src/icc/bufsearch/extract.py
+++ src/icc/bufsearch/extract.py
@@ -1,5 +1,21 @@
 import olefile
 import io
+import pkg_resources
+
+
+def resource(filename):
+    return pkg_resources.resource_filename("icc.bufsearch",
+                                           "../data/" + filename)
+
+
+TEMPLATES = {
+    "doc": "template-lo.doc",
+    "xls": "template-lo.xls",
+    "ppt": "template-lo.ppt"
+}
+
+STREAMMAP = {"WordDocument": "doc"}
+
 
 def extract_ole(buffer, pos, pattern=None):
     if type(pos) in [list, tuple]:
@@ -8,18 +24,30 @@ def extract_ole(buffer, pos, pattern=None):
             if pattern is None:
                 rc.append(extract_ole(buffer, p))
             else:
-                extract_ole(buffer, p, str(i)+pattern)
+                extract_ole(buffer, p, str(i) + pattern)
         return rc
 
     i = io.BytesIO(buffer[pos:])
-    olefile.isOleFile(i)
-    if pattern is None:
-        o=io.BytesIO()
+    if not olefile.isOleFile(i):
+        raise ValueError("BAD OLE file")
+    olei = olefile.OleFileIO(i, write_mode=False)
+    listdir = olei.listdir()
+    print(listdir)
+    for k, ext in STREAMMAP.items():
+        print (k)
+        if [k] in listdir:
+            break
     else:
-        o=open(pattern,"wb")
-    olei=olefile.OleFileIO(i, write_mode=False)
-    oleo=olefile.OleFileIO(o, write_mode=True)
-    for name in olei.listdir():
+        raise RuntimeError("format is not supported yet")
+    template = TEMPLATES[ext]
+    if pattern is None:
+        _data = open(resource(template),"rb").read()
+        o = io.BytesIO(_data)
+    else:
+        # TODO Copy template at first
+        o = open(pattern, "wb")
+    oleo = olefile.OleFileIO(o, write_mode=True)
+    for name in listdir:
         si = olei.openstream(name)
         data = si.read()
         oleo.write_stream(name, data)
--- src/tests/test_basic.py
+++ src/tests/test_basic.py
@@ -1,22 +1,26 @@
 from nose.tools import *
 import binascii as ba
 
-import icc.bufsearch as bs # ;-)
+import icc.bufsearch as bs  # ;-)
 from icc.bufsearch.extract import extract_ole
 import os
 import pkg_resources
 import olefile
 
+
 def res(filename):
-    return pkg_resources.resource_filename("icc.bufsearch", "../../tests/data/"+filename)
+    return pkg_resources.resource_filename("icc.bufsearch",
+                                           "../../tests/data/" + filename)
+
 
 PATTERN = b"pattern"
 PATTERN2 = b"abddb"
 UPATTERN = "pattern"
 
+
 class test_engine:
     def setUp(self):
-        self.raita=bs.Raita(PATTERN, multibuffer=False)
+        self.raita = bs.Raita(PATTERN, multibuffer=False)
 
     def tearDown(self):
         del self.raita
@@ -26,34 +30,34 @@ class test_engine:
         self.raita.search(UPATTERN)
 
     def test_equal(self):
-        rc, reason =self.raita.search(PATTERN)
+        rc, reason = self.raita.search(PATTERN)
         assert rc == [0]
-        assert reason=="found"
+        assert reason == "found"
 
     def test_double(self):
-        rc, reason =self.raita.search(PATTERN*2)
-        assert rc == [0,len(PATTERN)]
+        rc, reason = self.raita.search(PATTERN * 2)
+        assert rc == [0, len(PATTERN)]
 
     def test_double_count1(self):
-        rc, reason =self.raita.search(PATTERN*2, count=1)
-        print (rc)
+        rc, reason = self.raita.search(PATTERN * 2, count=1)
+        print(rc)
         assert rc == [0]
 
 
 class test_engine_multibuffer:
     def setUp(self):
-        self.raita=bs.Raita(PATTERN2, multibuffer=True)
+        self.raita = bs.Raita(PATTERN2, multibuffer=True)
 
     def tearDown(self):
         del self.raita
 
     def test_from_ex(self):
-        p=PATTERN2
+        p = PATTERN2
         rc1 = self.raita.search(p)
         #print (rc1)
 
     def test_from_ex_full(self):
-        p=b"abbaabaabddbabadbb"
+        p = b"abbaabaabddbabadbb"
         rc1 = self.raita.search(p)
         # print (rc1)
 
@@ -62,23 +66,25 @@ class test_engine_multibuffer:
     #     rc2 = self.raita.search(b"cd")
     #     print (rc1, rc2)
 
+
 DOC = "d0cf11e0a1b11ae1"
-DOC_PREFIX=ba.unhexlify(DOC)
+DOC_PREFIX = ba.unhexlify(DOC)
 
-files=["f1_25.07.doc","f2.doc","f1.doc"]
+files = ["f1_25.07.doc", "f2.doc", "f1.doc"]
 
-HEADER=b"Multielemental analysis of rocks with high calcium content by X-ray fluorescence spectrometry for environmental"
+HEADER = b"Multielemental analysis of rocks with high calcium content by X-ray fluorescence spectrometry for environmental"
 
-HEADER2=[]
+HEADER2 = []
 for i in HEADER:
     HEADER2.append(i)
     HEADER2.append(0)
 
-HEADER2=bytes(HEADER2)
+HEADER2 = bytes(HEADER2)
+
 
 class test_engine_with_file_data:
     def setUp(self):
-        self.raita=bs.Raita(DOC_PREFIX, multibuffer=False)
+        self.raita = bs.Raita(DOC_PREFIX, multibuffer=False)
         pass
 
     def tearDown(self):
@@ -87,20 +93,20 @@ class test_engine_with_file_data:
 
     def test_from_ex(self):
         for name in files:
-            name=res(name)
-            doc1=open(name,"rb")
-            data=doc1.read()
+            name = res(name)
+            doc1 = open(name, "rb")
+            data = doc1.read()
             assert data.startswith(DOC_PREFIX)
-            rc,_=self.raita.search(data)
+            rc, _ = self.raita.search(data)
             #print ("Data:", repr(data[:len(DOC_PREFIX)]))
             #print ("Patt:", repr(DOC_PREFIX))
             #print (rc)
-            assert rc.pop(0)==0
+            assert rc.pop(0) == 0
+
 
 class test_header_search:
-
     def setUp(self):
-        self.raita=bs.Raita(HEADER, multibuffer=False)
+        self.raita = bs.Raita(HEADER, multibuffer=False)
         pass
 
     def tearDown(self):
@@ -108,29 +114,29 @@ class test_header_search:
         pass
 
     def test_header_ok(self):
-        name=res(files[0])
-        data=open(name,"rb").read()
+        name = res(files[0])
+        data = open(name, "rb").read()
         rc, _ = self.raita.search(data)
         rc.pop(0)
 
     def test_header_no1(self):
-        name=res(files[1])
-        data=open(name,"rb").read()
+        name = res(files[1])
+        data = open(name, "rb").read()
         rc, _ = self.raita.search(data)
-        assert rc==None
+        assert rc == None
 
     def test_header_no2(self):
-        name=res(files[2])
-        data=open(name,"rb").read()
+        name = res(files[2])
+        data = open(name, "rb").read()
         rc, _ = self.raita.search(data)
-        assert rc==None
+        assert rc == None
 
 # print (repr(HEADER2))
 
-class test_header_search_2:
 
+class test_header_search_2:
     def setUp(self):
-        self.raita=bs.Raita(HEADER2, multibuffer=False)
+        self.raita = bs.Raita(HEADER2, multibuffer=False)
         pass
 
     def tearDown(self):
@@ -138,45 +144,47 @@ class test_header_search_2:
         pass
 
     def test_header_ok(self):
-        name=res(files[0])
-        data=open(name,"rb").read()
+        name = res(files[0])
+        data = open(name, "rb").read()
         rc, _ = self.raita.search(data)
         # print (rc)
         assert rc
 
     def test_header_no1(self):
-        name=res(files[1])
-        data=open(name,"rb").read()
+        name = res(files[1])
+        data = open(name, "rb").read()
         rc, _ = self.raita.search(data)
         assert rc
 
     def test_header_no2(self):
-        name=res(files[2])
-        data=open(name,"rb").read()
+        name = res(files[2])
+        data = open(name, "rb").read()
         rc, _ = self.raita.search(data)
         assert rc
 
-RAND_SIZE=40000
+
+RAND_SIZE = 40000
+
 
 class TestCaseFinding:
     def setUp(self):
         self.name = res(files[0])
-        self.buffer=[0,0]
-        self.buffer[0]=os.urandom(RAND_SIZE)+b"\0x1"
-        self.buffer[1]=os.urandom(RAND_SIZE)+b"\0x2"
-        self.content=open(self.name,"rb").read()
-        self.obfusc=self.buffer[0]+self.content+self.buffer[1]
-        self.raita=bs.Raita(DOC_PREFIX, multibuffer=False)
+        self.buffer = [0, 0]
+        self.buffer[0] = os.urandom(RAND_SIZE) + b"\0x1"
+        self.buffer[1] = os.urandom(RAND_SIZE) + b"\0x2"
+        self.content = open(self.name, "rb").read()
+        self.obfusc = self.buffer[0] + self.content + self.buffer[1]
+        self.raita = bs.Raita(DOC_PREFIX, multibuffer=False)
 
     def tearDown(self):
         pass
 
     def test_stub(self):
-        assert self.buffer[0]!=self.buffer[1]
+        assert self.buffer[0] != self.buffer[1]
 
     def test_search(self):
         rc, _ = self.raita.search(self.obfusc)
         offs = rc[0]
-        assert offs==len(self.buffer[0])
+        assert offs == len(self.buffer[0])
         buf = extract_ole(self.obfusc, rc)
-        assert len(buf[0])>0
+        assert len(buf[0]) > 0
